f8f563af7b5114723bef45e4673ec0e44c64b6f7
Duong Minh Quan <91663460+2uanDM@users.noreply.github.com>
2024-05-05 21:41:13 +0700

    feat: adding some constants

---
+++ src/utils/constants.py
@@ -0,0 +1,11 @@
+MOTOR_CLASSES = ["Xe Số", "Xe Ga", "N/A"]
+
+# Color for drawing bounding boxes of detections
+COLOR = {
+    0: [172, 47, 117],  # Xe số
+    1: [192, 67, 251],  # Xe ga
+    2: [195, 103, 9],  # N/A
+}
+
+# Blending the mask image with the original image
+MASK_ALPHA = 0.3
